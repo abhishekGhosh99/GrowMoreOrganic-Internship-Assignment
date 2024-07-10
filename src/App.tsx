56d310f2e057f635df659b3b3d8c6aaad3562a0a
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2024-07-11 02:39:43 +0530

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import UserForm from "./components/UserForm";
 import Data from "./components/Data";
